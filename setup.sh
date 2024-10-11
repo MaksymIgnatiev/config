29c52194c37fd435c2ee18b708e4e28500f53514
MaksymIgnatievOfficial <maksym.ignatiev2006@gmail.com>
2024-10-11 22:10:47 +0200

    Added kitty configuration + changed zsh configuration + changed setup script

--- setup.sh
+++ setup.sh
@@ -6,24 +6,24 @@ create_symlink() {
 
   if [ -e "$target" ]; then
 	  echo -e "\033[33m$target\033[0m already exists."
-	  read -p "Do you want to (\033[33mR\033[0m)eplace or (\033[33mS\033[0m)kip this file? " choice
+	  read -p "Do you want to (R)eplace or (S)kip this file? (default=skip) " choice
 	  case "$choice" in
-		  R|r)
-			  echo -e "Replacing \033[33m$target\033[0m"
+		  R|r|Replace|replace)
+			  echo -e "Replacing \033[32m$target\033[0m"
 			  rm -f "$target"
 			  ln -s "$source" "$target"
-			  echo -e "Created symlink for \033[33m$target\033[0m"
+			  echo -e "Created symlink for \033[32m$target\033[0m"
 			  ;;
-		  S|s)
-			  echo -e "Skipping $\033[33mtarget\033[0m"
+		  S|s|Skip|skip"")
+			  echo -e "Skipping \033[32m$target\033[0m"
 			  ;;
 		  *)
-			  echo -e "Invalid option. Skipping \033[33m$target\033[0m"
+			  echo -e "Invalid option. Skipping \033[32m$target\033[0m"
 			  ;;
 	  esac
   else
 	  ln -s "$source" "$target"
-	  echo -e "Created symlink for \033[33m$target\033[0m"
+	  echo -e "Created symlink for \033[32m$target\033[0m"
   fi
 }
 
@@ -35,13 +35,13 @@ create_symlink ~/config/zsh/.zsh_variables ~/.config/zsh/.zsh_variables
 create_symlink ~/config/zsh/.zsh_path ~/.config/zsh/.zsh_path
 create_symlink ~/config/zsh/.zsh_functions ~/.config/zsh/.zsh_functions
 
-create_symlink ~/config/zsh/.p10k.zsh ~/.p10k.zsh
+#create_symlink ~/config/zsh/.p10k.zsh ~/.p10k.zsh
 
 # Tmux configuration
-create_symlink ~/config/tmux/.tmux.conf ~/.tmux.conf
+#create_symlink ~/config/tmux/.tmux.conf ~/.tmux.conf
 
 # Kitty configuration
-create_symlink ~/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+# create_symlink ~/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 
 # i3 configuration
-create_symlink ~/config/i3/config ~/.config/i3/config
+# create_symlink ~/config/i3/config ~/.config/i3/config
