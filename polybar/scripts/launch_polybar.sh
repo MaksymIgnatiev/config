460a943a5d83c8fb904331bbc60baaf88b6a4ecf
MaksymIgnatiev <maksym.ignatiev2006@gmail.com>
2025-05-12 22:21:33 +0200

    [polybar/scripts/launch_polybar.sh] Wrapped all logic into function to supress all output

--- polybar/scripts/launch_polybar.sh
+++ polybar/scripts/launch_polybar.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-type "xrandr" && {
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-		MONITOR=$m polybar --reload toph &
-	done
-} || polybar --reload toph &
+main() {
+	type "xrandr" >/dev/null && {
+		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+			MONITOR=$m polybar --reload toph &
+		done
+	} || polybar --reload toph &
+}
+
+main >/dev/null 2>&1
